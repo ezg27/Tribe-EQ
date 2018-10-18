a85007966144b084cc9107626cbaf5e759eccc6a
ezg27 <georgehams27@gmail.com>
2018-10-18 13:56:46 +0100

    create Interface component

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Interface from './components/Interface';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello there</h1>
+        <h1>Tribe EQ</h1>
+        <Interface />
       </div>
     );
   }
---
+++ UI/src/components/Interface.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/Interface.css';
+
+class Interface extends Component {
+  render() {
+    return (
+      <div className='Interface-container'>
+        <h1>Hello there</h1>
+      </div>
+    );
+  }
+}
+
+export default Interface;
