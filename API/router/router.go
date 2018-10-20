91886188af84b5d3cb8ca331a7c59dc2ef62321f
ezg27 <georgehams27@gmail.com>
2018-10-20 16:35:10 +0100

    uncomment GetAllPresets route

--- API/router/router.go
+++ API/router/router.go
@@ -2,12 +2,12 @@ package router
 
 import (
 	"github.com/labstack/echo"
-	// "github.com/ezg27/Tribe-EQ/API/controllers"
+	"github.com/ezg27/Tribe-EQ/API/controllers"
 )
 
 // Init : Create endpoints
 func Init(e *echo.Echo) {
-	// e.GET("/api/presets", controllers.GetAllPresets)
+	e.GET("/api/presets", controllers.GetAllPresets)
 	// e.GET("/api/presets/:id", controllers.GetPresetByID)
 	// e.POST("/api/presets/:id", controllers.CreatePreset)
 	// e.PATCH("/api/presets/:id", controllers.UpdatePreset)
