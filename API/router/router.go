6b92e1cfe9083cec83505212ff454ed4f474d96d
ezg27 <georgehams27@gmail.com>
2018-10-20 15:08:11 +0100

    create server endpoints

---
+++ API/router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/labstack/echo"
+	// "github.com/ezg27/Tribe-EQ/API/controllers"
+)
+
+// Init : Create endpoints
+func Init(e *echo.Echo) {
+	// e.GET("/api/presets", controllers.GetAllPresets)
+	// e.GET("/api/presets/:id", controllers.GetPresetById)
+	// e.POST("/api/presets/:id", controllers.CreatePreset)
+	// e.PATCH("/api/presets/:id", controllers.UpdatePreset)
+	// e.DELETE("/api/presets/:id", controllers.DeletePreset)
+}
