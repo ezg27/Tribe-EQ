af6b42828aa0238cda8b51168a7e6bf4720005f7
ezg27 <georgehams27@gmail.com>
2018-10-20 15:27:02 +0100

    create initial controller functions

---
+++ API/controllers/controllers.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+
+	"github.com/labstack/echo"
+	// "net/http"
+)
+
+// GetAllPresets function
+func GetAllPresets(c echo.Context) error {
+	
+}
+
+// GetPresetByID function
+func GetPresetByID(c echo.Context) error {
+
+}
+
+// CreatePreset function
+func CreatePreset(c echo.Context) error {
+
+}
+
+// UpdatePreset function
+func UpdatePreset(c echo.Context) error {
+
+}
+
+// DeletePreset function
+func DeletePreset(c echo.Context) error {
+
+}
--- API/router/router.go
+++ API/router/router.go
@@ -8,7 +8,7 @@ import (
 // Init : Create endpoints
 func Init(e *echo.Echo) {
 	// e.GET("/api/presets", controllers.GetAllPresets)
-	// e.GET("/api/presets/:id", controllers.GetPresetById)
+	// e.GET("/api/presets/:id", controllers.GetPresetByID)
 	// e.POST("/api/presets/:id", controllers.CreatePreset)
 	// e.PATCH("/api/presets/:id", controllers.UpdatePreset)
 	// e.DELETE("/api/presets/:id", controllers.DeletePreset)
