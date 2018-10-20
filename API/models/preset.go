348e06bd896da0903e1c52351ff4309e215be34c
ezg27 <georgehams27@gmail.com>
2018-10-20 16:42:22 +0100

    add ObjectId to preset model

--- API/models/preset.go
+++ API/models/preset.go
@@ -1,7 +1,10 @@
 package models
 
+import "gopkg.in/mgo.v2/bson"
+
 // Preset struct
 type Preset struct {
+	ID bson.ObjectId `json:"id" bson:"_id,omitempty"`
 	Name string `bson:"name"`
 	LowBand struct {
 		OnOff bool `json:"on/off" bson:"on/off"`
