3cf6a8e9854a1b94860a368991258ef3ba7e9d38
ezg27 <georgehams27@gmail.com>
2018-10-20 16:34:18 +0100

    create GetAllPresets controller function

--- API/controllers/controllers.go
+++ API/controllers/controllers.go
@@ -1,32 +1,34 @@
 package controllers
 
 import (
-
 	"github.com/labstack/echo"
-	// "net/http"
+	// preset "github.com/ezg27/Tribe-EQ/API/models"
+	"github.com/ezg27/Tribe-EQ/API/dao"
+	"net/http"
 )
 
 // GetAllPresets function
 func GetAllPresets(c echo.Context) error {
-	
+	res, _ := dao.GetAll()
+	return c.JSON(http.StatusOK, res)
 }
 
 // GetPresetByID function
-func GetPresetByID(c echo.Context) error {
+// func GetPresetByID(c echo.Context) error {
 
-}
+// }
 
-// CreatePreset function
-func CreatePreset(c echo.Context) error {
+// // CreatePreset function
+// func CreatePreset(c echo.Context) error {
 
-}
+// }
 
-// UpdatePreset function
-func UpdatePreset(c echo.Context) error {
+// // UpdatePreset function
+// func UpdatePreset(c echo.Context) error {
 
-}
+// }
 
-// DeletePreset function
-func DeletePreset(c echo.Context) error {
+// // DeletePreset function
+// func DeletePreset(c echo.Context) error {
 
-}
+// }
