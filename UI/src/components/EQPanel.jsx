3acb9006de495afc6ae085600bc4d5208a756b62
ezg27 <georgehams27@gmail.com>
2018-10-21 22:06:00 +0100

    add band components to EQPanel

--- UI/src/components/EQPanel.jsx
+++ UI/src/components/EQPanel.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import LowBand from './LowBand';
+import LowMidBand from './LowMidBand';
+import HighMidBand from './HighMidBand';
+import HighBand from './HighBand';
 import '../css/EQPanel.css';
 
 class EQPanel extends Component {
+  state = {
+    value: 0
+  };
   render() {
     return (
-      <div className='EQPanel-container'>
-        <h2>Panel</h2>
+      <div className="EQPanel-container">
+        <LowBand />
+        <LowMidBand />
+        <HighMidBand />
+        <HighBand />
       </div>
     );
   }
