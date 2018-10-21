f4ce20b219234600380038cc8869428d6c2c7ac0
ezg27 <georgehams27@gmail.com>
2018-10-21 16:49:04 +0100

    adjust title layout and styling

--- UI/src/App.js
+++ UI/src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Tribe EQ</h1>
+        <div className='Title-container'>
+          <h1>Tribe EQ</h1>
+        </div>
         <Interface />
       </div>
     );
