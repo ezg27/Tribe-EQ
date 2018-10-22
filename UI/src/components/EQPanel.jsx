6c6ebf0d21f43907714a3332b55ffaebe70b9ed8
ezg27 <georgehams27@gmail.com>
2018-10-22 11:46:13 +0100

    store freqVals in state + implement mapping of Band components

--- UI/src/components/EQPanel.jsx
+++ UI/src/components/EQPanel.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import LowBand from './LowBand';
-import LowMidBand from './LowMidBand';
-import HighMidBand from './HighMidBand';
-import HighBand from './HighBand';
+import Band from './Band';
 import '../css/EQPanel.css';
 
 class EQPanel extends Component {
@@ -14,16 +11,58 @@ class EQPanel extends Component {
       max: 15,
       default: 0,
       unit: 'dB'
-    }
+    },
+    freqVals: [
+      {
+        title: 'Freq',
+        min: 30,
+        max: 450,
+        default: 60,
+        unit: 'Hz',
+        color: 'rgb(60, 60, 60)'
+      },
+      {
+        title: 'Freq',
+        min: 200,
+        max: 2500,
+        default: 500,
+        unit: 'Hz',
+        color: 'rgb(49, 114, 255)'
+      },
+      {
+        title: 'Freq',
+        min: 600,
+        max: 7000,
+        default: 3000,
+        unit: 'Hz',
+        color: 'rgb(22, 186, 22)'
+      },
+      {
+        title: 'Freq',
+        min: 1500,
+        max: 16000,
+        default: 8000,
+        unit: 'Hz',
+        color: 'rgb(222, 59, 59)'
+      }
+    ]
   };
   render() {
-    const { gainVals } = this.state;
+    const {
+      gainVals,
+      freqVals
+    } = this.state;
     return (
       <div className="EQPanel-container">
-        <LowBand gainVals={gainVals} />
-        <LowMidBand gainVals={gainVals} />
-        <HighMidBand gainVals={gainVals} />
-        <HighBand gainVals={gainVals} />
+        {freqVals.map((band, i) => {
+          return (
+            <Band
+              gainVals={gainVals}
+              freqVals={band}
+              style={{ gridColumn: i + 1 }}
+            />
+          );
+        })}
       </div>
     );
   }
