f0a40384ccfe29b3fdd7cbd1b13bfffb345b1b02
ezg27 <georgehams27@gmail.com>
2018-10-22 14:02:37 +0100

    remove trailing slash with echo middleware

--- API/router/router.go
+++ API/router/router.go
@@ -3,10 +3,12 @@ package router
 import (
 	"github.com/ezg27/Tribe-EQ/API/controllers"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // Init : Create endpoints
 func Init(e *echo.Echo) {
+	e.Pre(middleware.RemoveTrailingSlash())
 	e.GET("/api/presets", controllers.GetAllPresets)
 	e.POST("/api/presets", controllers.CreatePreset)
 	e.GET("/api/presets/:id", controllers.GetPresetByID)
