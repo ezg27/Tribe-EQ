b87d2b6a68ab8c3d1406221958140a993dd89225
ezg27 <georgehams27@gmail.com>
2018-10-22 13:03:01 +0100

    add switches to Band component

--- UI/src/components/Band.jsx
+++ UI/src/components/Band.jsx
@@ -1,15 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MySlider from './MySlider';
+import Switch from 'react-switch';
 import 'rc-slider/assets/index.css';
 import '../css/Bands.css';
 
-const Band = ({ gainVals, freqVals }) => {
-  return (
-    <div className="Band-container">
-      <MySlider scale={gainVals} trackColor={freqVals.color} />
-      <MySlider scale={freqVals} trackColor={freqVals.color} />
-    </div>
-  );
-};
+class Band extends Component {
+  state = {
+    onOff: false,
+    eqParam: false
+  };
+  render() {
+    const { gainVals, freqVals } = this.props;
+    return (
+      <div className="Band-container">
+        <MySlider
+          scale={gainVals}
+          trackColor={freqVals.color}
+          style={{ gridColumn: 1 }}
+        />
+        <MySlider
+          scale={freqVals}
+          trackColor={freqVals.color}
+          style={{ gridColumn: 2 }}
+        />
+        <Switch
+          onChange={this.handleOnOffChange}
+          checked={this.state.onOff}
+          onColor="#86d3ff"
+          onHandleColor="#2693e6"
+          handleDiameter={22}
+          uncheckedIcon={false}
+          checkedIcon={false}
+          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+          height={15}
+          width={36}
+          className="Param-switch"
+          id="Param-switch"
+        />
+        <p
+          className="Switch-labels"
+          for="EQParam-switch"
+          style={{ gridRow: 2, gridColumn: 1 }}
+        >
+          Peak/Shelf
+        </p>
+        <Switch
+          onChange={this.handleEQChange}
+          checked={this.state.eqParam}
+          onColor="#86d3ff"
+          onHandleColor="#2693e6"
+          handleDiameter={22}
+          uncheckedIcon={false}
+          checkedIcon={false}
+          boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+          activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+          height={15}
+          width={36}
+          className="OnOff-switch"
+          id="OnOff-switch"
+        />
+        <p
+          className="Switch-labels"
+          for="OnOff-switch"
+          style={{ gridRow: 2, gridColumn: 2 }}
+        >
+          On/Off
+        </p>
+      </div>
+    );
+  }
+  handleOnOffChange = checked => {
+    this.setState({ onOff: checked });
+  };
+  handleEQChange = checked => {
+    this.setState({ eqParam: checked });
+  };
+}
 
 export default Band;
