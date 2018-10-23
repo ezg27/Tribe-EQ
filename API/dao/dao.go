e504528de3775afdf482ce4d2c38f6b262c1d5e8
ezg27 <georgehams27@gmail.com>
2018-10-23 13:51:11 +0100

    refactor GetAll + GetByID functions to match config

--- API/dao/dao.go
+++ API/dao/dao.go
@@ -9,112 +9,86 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-const collection string = "presets"
-
 // GetAll : return all presets from database
 func GetAll() (models.Presets, error) {
-	db := config.DB{}
 	res := models.Presets{}
-
-	session, err := db.DoDial()
-	if err != nil {
-		return res, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to connect to database")
-	}
-
-	defer session.Close()
-
-	c := session.DB(db.Name()).C(collection)
-
-	err = c.Find(bson.M{}).All(&res)
+	err := config.Presets.Find(bson.M{}).All(&res)
 	if err != nil {
 		return res, echo.NewHTTPError(http.StatusNotFound, "Error: Unable to find presets data")
 	}
-
 	return res, err
 }
 
 // GetByID : return preset with passed ID
 func GetByID(id string) (models.Preset, error) {
-	db := config.DB{}
 	res := models.Preset{}
-
-	session, err := db.DoDial()
-	if err != nil {
-		return res, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to connect to database")
-	}
-
-	defer session.Close()
-
-	c := session.DB(db.Name()).C(collection)
-
-	err = c.Find(bson.M{"_id": bson.ObjectIdHex(id)}).One(&res)
+	err := config.Presets.Find(bson.M{"_id": bson.ObjectIdHex(id)}).One(&res)
 	if err != nil {
 		return res, echo.NewHTTPError(http.StatusNotFound, "Error: Unable to find preset")
 	}
-
 	return res, err
 }
 
-// CreatePreset : add new preset to database
-func CreatePreset(p models.Preset) (models.Preset, error) {
-	db := config.DB{}
+// // CreatePreset : add new preset to database
+// func CreatePreset(p models.Preset) (models.Preset, error) {
+// 	db := config.DB{}
 
-	session, err := db.DoDial()
-	if err != nil {
-		return p, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to connect to database")
-	}
+// 	session, err := db.DoDial()
+// 	if err != nil {
+// 		return p, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to connect to database")
+// 	}
 
-	defer session.Close()
+// 	defer session.Close()
 
-	c := session.DB(db.Name()).C(collection)
+// 	c := session.DB(db.Name()).C(collection)
 
-	err = c.Insert(p)
-	if err != nil {
-		return p, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to insert preset into database")
-	}
+// 	err = c.Insert(p)
+// 	if err != nil {
+// 		return p, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to insert preset into database")
+// 	}
 
-	return p, err
-}
+// 	return p, err
+// }
 
-// UpdatePreset : update existing preset in database
-func UpdatePreset(id string, p models.Preset) (models.Preset, error) {
-	db := config.DB{}
+// // UpdatePreset : update existing preset in database
+// func UpdatePreset(id string, p models.Preset) (models.Preset, error) {
+// 	db := config.DB{}
 
-	session, err := db.DoDial()
-	if err != nil {
-		return p, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to connect to database")
-	}
+// 	session, err := db.DoDial()
+// 	if err != nil {
+// 		return p, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to connect to database")
+// 	}
 
-	defer session.Close()
+// 	defer session.Close()
 
-	c := session.DB(db.Name()).C(collection)
+// 	c := session.DB(db.Name()).C(collection)
 
-	err = c.Update(bson.M{}, p)
-	if err != nil {
-		return p, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to update preset")
-	}
+// 	err = c.Update(bson.M{}, p)
+// 	if err != nil {
+// 		return p, echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to update preset")
+// 	}
 
-	return p, err
-}
+// 	return p, err
+// }
 
-// DeletePreset : remove preset from database
-func DeletePreset(id string) error {
-	db := config.DB{}
+// // DeletePreset : remove preset from database
+// func DeletePreset(id string) error {
+// 	db := config.DB{}
 
-	session, err := db.DoDial()
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to connect to database")
-	}
+// 	session, err := db.DoDial()
+// 	if err != nil {
+// 		return echo.NewHTTPError(http.StatusInternalServerError, "Error: Unable to connect to database")
+// 	}
 
-	idoi := bson.ObjectIdHex(id)
+// 	idoi := bson.ObjectIdHex(id)
 
-	defer session.Close()
+// 	defer session.Close()
 
-	c := session.DB(db.Name()).C(collection)
+// 	c := session.DB(db.Name()).C(collection)
 
-	err = c.RemoveId(idoi)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusNotFound, "Error: Unable to delete preset from database")
-	}
-	return err
-}
+// 	err = c.RemoveId(idoi)
+// 	if err != nil {
+// 		return echo.NewHTTPError(http.StatusNotFound, "Error: Unable to delete preset from database")
+// 	}
+// 	return err
+// }
