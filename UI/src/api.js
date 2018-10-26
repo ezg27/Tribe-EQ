3df4de9fca2d383d1f19857e1973e63884a8173b
ezg27 <georgehams27@gmail.com>
2018-10-26 14:28:43 +0100

    create api functions for other endpoints

--- UI/src/api.js
+++ UI/src/api.js
@@ -4,3 +4,19 @@ const DB_URL = 'https://tribe-eq.herokuapp.com';
 export const fetchPresets = () => {
   return axios.get(`${DB_URL}/api/presets`).then(({ data }) => data.presets);
 };
+
+export const fetchPresetByID = id => {
+  return axios.get(`${DB_URL}/api/presets/${id}`).then(({ data }) => data);
+};
+
+export const createPreset = preset => {
+  return axios.post(`${DB_URL}/api/presets`, preset).then(({ data }) => data);
+};
+
+export const updatePreset = id => {
+  return axios.patch(`${DB_URL}/api/presets/${id}`).then(({ data }) => data);
+};
+
+export const deletePreset = id => {
+  return axios.delete(`${DB_URL}/api/presets/${id}`).then(({ data }) => data);
+};
