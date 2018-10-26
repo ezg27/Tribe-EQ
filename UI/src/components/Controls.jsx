5e8d936b0dbde1b460f68e088d3e2ece5a9c6448
ezg27 <georgehams27@gmail.com>
2018-10-26 16:07:06 +0100

    add handleUpdate + handleDelete functions

--- UI/src/components/Controls.jsx
+++ UI/src/components/Controls.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import * as api from '../api';
 import '../css/PresetList.css';
 
 class Controls extends Component {
   render() {
     return (
-      <div className='Controls-container'>
-        <button className='Control-button'>Update Preset</button>
-        <button className='Control-button'>Create New Preset</button>
-        <button className='Control-button'>Delete Current Preset</button>
+      <div className="Controls-container">
+        <button className="Control-button" onClick={this.handleUpdate}>
+          Update Preset
+        </button>
+        <button className="Control-button" onClick={this.handleCreate}>
+          Create New Preset
+        </button>
+        <button className="Control-button" onClick={this.handleDelete}>
+          Delete Current Preset
+        </button>
       </div>
     );
   }
+  handleUpdate = () => {
+    const { currentPreset } = this.props;
+    api.updatePreset(currentPreset).then(response => {
+      console.log(response);
+    });
+  };
+  handleDelete = () => {
+    const { currentPreset, recallPresets } = this.props;
+    api.deletePreset(currentPreset.id).then(response => {
+      console.log(response);
+      recallPresets();
+    });
+  };
 }
 
 export default Controls;
