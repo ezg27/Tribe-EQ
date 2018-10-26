446596e6aeac37c53570ad2f1e86e89593e74d8b
ezg27 <georgehams27@gmail.com>
2018-10-26 20:43:25 +0100

    add error handling to api functions

--- UI/src/api.js
+++ UI/src/api.js
@@ -1,24 +1,31 @@
 import axios from 'axios';
 const DB_URL = 'https://tribe-eq.herokuapp.com';
 
-export const fetchPresets = () => {
+const errorHandling = apiFunc => {
+  return (...args) =>
+    apiFunc(...args).catch(err => {
+      return { type: 'error', message: err.message };
+    });
+};
+
+export const fetchPresets = errorHandling(() => {
   return axios.get(`${DB_URL}/api/presets`).then(({ data }) => data.presets);
-};
+});
 
-export const fetchPresetByID = id => {
+export const fetchPresetByID = errorHandling(id => {
   return axios.get(`${DB_URL}/api/presets/${id}`).then(({ data }) => data);
-};
+});
 
-export const createPreset = preset => {
+export const createPreset = errorHandling(preset => {
   return axios.post(`${DB_URL}/api/presets`, preset).then(({ data }) => data);
-};
+});
 
-export const updatePreset = preset => {
+export const updatePreset = errorHandling(preset => {
   return axios
     .patch(`${DB_URL}/api/presets/${preset.id}`, preset)
     .then(({ data }) => data);
-};
+});
 
-export const deletePreset = id => {
+export const deletePreset = errorHandling(id => {
   return axios.delete(`${DB_URL}/api/presets/${id}`).then(({ data }) => data);
-};
+});
