ab402d5b31c38a323685676a68494c27d6c3a834
ezg27 <georgehams27@gmail.com>
2018-10-26 11:14:34 +0100

    remove unnecessary div + add componentDidUpdate

--- UI/src/components/PresetList.jsx
+++ UI/src/components/PresetList.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Controls from './Controls';
 import '../css/PresetList.css';
 
 class PresetList extends Component {
@@ -10,33 +9,38 @@ class PresetList extends Component {
   render() {
     const { currentPreset } = this.state;
     return (
-      <div>
-        <div className="PresetList-container">
-          <h3>Preset List</h3>
-          <ul className="presets-list">
-            {this.props.presets.map((preset, i) => {
-              return (
-                <li key={preset.id}>
-                  <div
-                    className="list-item"
-                    style={
-                      (currentPreset === null && !i) || currentPreset === preset
-                        ? { backgroundColor: 'rgb(169, 169, 169)' }
-                        : null
-                    }
-                    onClick={() => this.setCurrentPreset(preset)}
-                  >
-                    <span style={{ float: 'right' }}>X</span>
-                    <p>{preset.name}</p>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <Controls />
+      <div className="PresetList-container">
+        <h3>Preset List</h3>
+        <ul className="presets-list">
+          {this.props.presets.map((preset, i) => {
+            return (
+              <li key={preset.id}>
+                <div
+                  className="list-item"
+                  style={
+                    (currentPreset === null && !i) || currentPreset === preset
+                      ? { backgroundColor: 'rgb(169, 169, 169)' }
+                      : null
+                  }
+                  onClick={() => this.setCurrentPreset(preset)}
+                >
+                  <span style={{ float: 'right' }}>X</span>
+                  <p>{preset.name}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
+  }
+  componentDidUpdate(prevProps) {
+    const { currentPreset } = this.props;
+    if (prevProps.currentPreset !== this.props.currentPreset) {
+      this.setState({
+        currentPreset
+      });
+    }
   }
   setCurrentPreset = preset => {
     this.props.passCurrentPreset(preset);
