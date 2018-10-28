4e1d0ff9906d95a8f0fd7edd8445e289f3b96e4e
ezg27 <georgehams27@gmail.com>
2018-10-28 15:43:25 +0000

    add PropTypes to PresetList

--- UI/src/components/PresetList.jsx
+++ UI/src/components/PresetList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import '../css/PresetList.css';
 
 class PresetList extends Component {
@@ -31,5 +32,11 @@ class PresetList extends Component {
     );
   }
 }
+
+
+PresetList.propTypes = {
+  currentPreset: PropTypes.object,
+  passCurrentPreset: PropTypes.func
+};
 
 export default PresetList;
