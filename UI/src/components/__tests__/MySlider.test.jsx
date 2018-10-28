668c4f38ef11302c48bfaa224a15148b67f7c7f2
ezg27 <georgehams27@gmail.com>
2018-10-28 21:44:03 +0000

    additional tests for MySlider

--- UI/src/components/__tests__/MySlider.test.jsx
+++ UI/src/components/__tests__/MySlider.test.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import MySlider from '../MySlider';
+import Slider from 'rc-slider';
 
-const band = {
-  // title: 'Freq',
-  // band: 'low_band',
-  // min: 30,
-  // max: 450,
-  // default: 60,
-  // unit: 'Hz',
-  // color: 'rgb(60, 60, 60)'
+const props = {
+  scale: {
+    title: 'Gain',
+    min: -150,
+    max: 150,
+    default: 0,
+    unit: 'dB'
+  },
+  value: 60,
+  trackColor: 'rgb(60, 60, 60)',
+  band: 'low_band',
+  handleGainChange: () => handleGainChange()
 };
 
+const { scale, value, trackColor, band, handleGainChange } = props;
+
 it('renders without crashing', () => {
-  shallow(<MySlider scale={band} />);
+  shallow(<MySlider {...props} />);
+});
+
+it('renders elements with values passed on props', () => {
+  let wrapper = shallow(<MySlider {...props} />);
+  expect(wrapper.hasClass('Slider-container')).toEqual(true);
+  expect(wrapper.find('h3').text()).toEqual(props.scale.title);
+  expect(wrapper.find(Slider).prop('min')).toEqual(scale.min);
+  expect(wrapper.find(Slider).prop('max')).toEqual(scale.max);
+  expect(wrapper.find(Slider).prop('value')).toEqual(value);
+  expect(wrapper.find(Slider).prop('trackStyle')).toEqual({
+    backgroundColor: trackColor
+  });
+  expect(wrapper.find(Slider).prop('band')).toEqual(band);
 });
