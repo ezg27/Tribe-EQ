c1c20f84dba71330a0ca252bd946d716ffab4e4a
ezg27 <georgehams27@gmail.com>
2018-10-28 23:16:21 +0000

    remove unused band data

--- UI/src/components/__tests__/Band.test.jsx
+++ UI/src/components/__tests__/Band.test.jsx
@@ -2,16 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Band from '../Band';
 
-const band = {
-  title: 'Freq',
-  band: 'low_band',
-  min: 30,
-  max: 450,
-  default: 60,
-  unit: 'Hz',
-  color: 'rgb(60, 60, 60)'
-};
-
 it('renders without crashing', () => {
-  shallow(<Band freqDefs={band}/>);
+  shallow(<Band freqDefs={{}}/>);
 });
