f20453232aa660b7d5d5489008cb6f427e3ee0bc
ezg27 <georgehams27@gmail.com>
2018-10-28 23:17:25 +0000

    add tests for Create New Preset button in Controls

--- UI/src/components/__tests__/Controls.test.jsx
+++ UI/src/components/__tests__/Controls.test.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Controls from '../Controls';
 
 it('renders without crashing', () => {
   shallow(<Controls />);
+});
+
+describe('Create New Preset button', () => {
+  it('renders Create New Preset button', () => {
+    let wrapper = shallow(<Controls />);
+    expect(
+      wrapper
+        .find('button')
+        .at(1)
+        .text()
+    ).toBe('Create New Preset');
+  });
+
+  it('Create New Preset button onClick function updates state', () => {
+    let onClick = jest.fn();
+    let wrapper = mount(<Controls onClick={onClick} />);
+    expect(wrapper.state('modalIsOpen')).toEqual(false);
+    wrapper
+      .find('button')
+      .at(1)
+      .simulate('click');
+    expect(wrapper.state('modalIsOpen')).toEqual(true);
+  });
 });
